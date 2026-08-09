df5567b70a644ae02694cadddb68b3d8446cbe87
Linamandla Mzamane <mzamanelinamandla@outlook.com>
2026-08-09 13:51:24 +0200

    Renamed data_loader.py to load_data.py

---
+++ src/insurance_charges_prediction/load_data.py
@@ -0,0 +1,11 @@
+"""
+This module contains logic for reading data and returning a clean data frame.
+
+Inspect: column names, missing values and dtypes. Check for trailing spaces in
+column names and formating issues in the data.
+"""
+
+import pandas as pd
+
+def load_raw_data():
+    df = pd.read_csv()
